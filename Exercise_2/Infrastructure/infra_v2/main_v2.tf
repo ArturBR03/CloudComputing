47f943fd626a74d6580dc1bf66befe8e98b5e769
Roshan-Raza Khalid <153830991+Raza217@users.noreply.github.com>
2025-06-20 12:44:20 +0200

    Add files via upload
    Add the second version of the Terraform infrastructure

---
+++ Exercise_2/Infrastructure/infra_v2/main_v2.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+    }
+  }
+}
+
+
+provider "openstack" {
+  auth_url    = "https://stack.dhbw.cloud:5000/v3"
+  domain_name ="default"
+  user_name   ="pfisterer-cloud-lecture"
+  tenant_id ="d11c8af5f24f4756a6d51b880162f71f"
+  password    ="ss2025"
+}
+
+
+resource "openstack_compute_instance_v2" "web_server_rk" {
+    name = "rk-demo-bigdata"
+    image_name = "Ubuntu 24.04 2025-01"
+    flavor_name = "cb1.small"
+    key_pair = "BIP-Key"
+    security_groups = ["default"]
+
+  network {
+    name = "provider_912"
+  }
+}
+
+resource "local_file" "floating_ip" {
+  content  = openstack_compute_instance_v2.web_server_rk.network.0.fixed_ip_v4
+  filename = "${path.module}/openstack-inventory.txt"
+}
