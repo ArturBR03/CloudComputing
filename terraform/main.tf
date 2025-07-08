cfab3c8ed77dc93f146a31109f63275467c2affa
Roshan-Raza Khalid <153830991+Raza217@users.noreply.github.com>
2025-07-08 15:42:57 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,24 +6,22 @@ terraform {
   }
 }
 
-
 provider "openstack" {
   auth_url    = "https://stack.dhbw.cloud:5000/v3"
-  domain_name ="default"
-  user_name   ="pfisterer-cloud-lecture"
-  tenant_id ="Your_Tenant_ID"
-  password    ="Your_Cloud_Password"
+  domain_name = "default"
+  user_name   = "pfisterer-cloud-lecture"
+  tenant_id   = "Your_Tenant_ID"      # Replace with actual tenant ID
+  password    = "Your_Cloud_Password" # Replace with actual password
 }
 
-
 resource "openstack_compute_instance_v2" "web_server_rk" {
-    name = "rk-demo-bigdata"
-    image_name = "Ubuntu 24.04 2025-01"
-    flavor_name = "cb1.medium"
-    key_pair = "RK"
-    security_groups = ["default"]
+  name            = "rk-demo-bigdata"
+  image_name      = "Ubuntu 24.04 2025-01" # Immutable core attribute
+  flavor_name     = "cb1.medium"
+  key_pair        = "RK"                  # Pre-uploaded SSH key
+  security_groups = ["default"]           # Firewall rules
 
   network {
-    name = "provider_912"
+    name = "provider_912"                 # Public network
   }
 }
